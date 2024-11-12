96e387877bf3e634650e3ea1324f7d872717b341
Aishwaryaanthati <148621647+Aishwaryaanthati@users.noreply.github.com>
2024-11-12 10:35:58 +0530

    Create Main.java

---
+++ eb_dsa/Main.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+public class Solution{
+  public static void main(String [] args){
+    Scanner sc=new Scanner(System.in);
+    int N=sc.nextInt();
+    if(N>=0){
+System.out.println("postive");
+    }else{
+      System.out.println("negative");
+    }
+  }
+}
