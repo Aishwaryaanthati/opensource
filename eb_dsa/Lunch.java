a796058d5e50223d143089b43f99378802960d40
Aishwaryaanthati <148621647+Aishwaryaanthati@users.noreply.github.com>
2024-11-14 21:45:40 +0530

    Create Lunch.java

---
+++ eb_dsa/Lunch.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int X=sc.nextInt();
+        if(X>=1 && X<=4){
+            System.out.println("Yes");
+        }else{
+            System.out.println("No");
+        }
+    }
+}
