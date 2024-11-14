2ae924c76915fa2561373aad75f104c10f474656
Aishwaryaanthati <148621647+Aishwaryaanthati@users.noreply.github.com>
2024-11-14 21:39:30 +0530

    Create Taller.java

---
+++ eb_dsa/Taller.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int T=sc.nextInt();
+        while(T-->0){
+            int X=sc.nextInt();
+            int Y=sc.nextInt();
+            if(X>Y){
+                System.out.println("A");
+            }else{
+                System.out.println("B");
+            }
+        }
+    }
+    }
