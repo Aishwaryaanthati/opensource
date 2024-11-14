7c3fc79607beeef72e82431cfbcb472063efd046
Aishwaryaanthati <148621647+Aishwaryaanthati@users.noreply.github.com>
2024-11-14 21:30:53 +0530

    Create Halloween.java

---
+++ eb_dsa/Halloween.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int N=sc.nextInt();
+        int M=sc.nextInt();
+        int X=sc.nextInt();
+        int Y=sc.nextInt();
+        int A=N*X;
+        int B=M*Y;
+        System.out.println(A+B);
+        
+    }
+}
